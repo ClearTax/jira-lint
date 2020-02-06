dce84e3840f1e5ac52caf48c4cd7a2da190c7229
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-02-06 18:02:38 +0530

    Utilize a simpler regex to match for JIRA key in branches (#2)

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -76,10 +76,12 @@ describe('getJIRAIssueKeys()', () => {
   it('gets multiple keys from a string', () => {
     expect(
       getJIRAIssueKeys('BF-18 abc-123 X-88 ABCDEFGHIJKL-999 abc XY-Z-333 abcDEF-33 ABCDEF-33 abcdef-33 ABC-1')
-    ).toEqual(['BF-18', 'abc-123', 'X-88', 'ABCDEFGHIJKL-999', 'ABCDEF-33', 'abcdef-33', 'ABC-1']);
+    ).toEqual(['BF-18', 'abc-123', 'X-88', 'CDEFGHIJKL-999', 'Z-333', 'DEF-33', 'ABCDEF-33', 'abcdef-33', 'ABC-1']);
   });
 
   it('gets jira key from different branch names', () => {
+    expect(getJIRAIssueKeys('fix/login-protocol-es-43')).toEqual(['es-43']);
+    expect(getJIRAIssueKeys('fix/login-protocol-ES-43')).toEqual(['ES-43']);
     expect(getJIRAIssueKeys('feature/newFeature_esch-100')).toEqual(['esch-100']);
     expect(getJIRAIssueKeys('feature/newFeature_ESCH-101')).toEqual(['ESCH-101']);
     expect(getJIRAIssueKeys('feature/newFeature--mojo-5611')).toEqual(['mojo-5611']);
@@ -87,10 +89,10 @@ describe('getJIRAIssueKeys()', () => {
 
     expect(getJIRAIssueKeys('chore/task-with-dashes--MOJO-6789')).toEqual(['MOJO-6789']);
     expect(getJIRAIssueKeys('chore/task_with_underscores--MOJO-6789')).toEqual(['MOJO-6789']);
+    expect(getJIRAIssueKeys('chore/MOJO-6789-task_with_underscores')).toEqual(['MOJO-6789']);
+    expect(getJIRAIssueKeys('MOJO-6789/task_with_underscores')).toEqual(['MOJO-6789']);
 
-    expect(getJIRAIssueKeys('bugfix/MOJO-6789--with-prefixed-id')).toEqual(['MOJO-6789']);
-    expect(getJIRAIssueKeys('MOJO-6789/without-branch-prefix-convention')).toEqual(['MOJO-6789']);
-    expect(getJIRAIssueKeys('MOJO-6789/without-branch-prefix-convention-1234')).toEqual(['MOJO-6789']);
+    expect(getJIRAIssueKeys('MOJO-6789/task_with_underscores-ES-43')).toEqual(['MOJO-6789', 'ES-43']);
 
     expect(getJIRAIssueKeys('feature/missingKey')).toEqual([]);
     expect(getJIRAIssueKeys('')).toEqual([]);
